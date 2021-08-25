4491d060e387559639b43424b30a66c1cb48f638
Oybek-Yuldoshev <88775680+Oybek-Yuldoshev@users.noreply.github.com>
2021-08-24 22:48:45 -0500

    Add files via upload

---
+++ L_python(hometask_1).py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('Assalomu alaykum barchaga!')
+
+
+# In[2]:
+
+
+print("Nega hamma jim bo'lib qoldi?")
+
+
+# In[6]:
+
+
+sana = 12
+oy = "Avgust"
+print("Bugun " + str(sana) + " " + oy)
+
+
+# In[7]:
+
+
+sana = 12
+oy = "Avgust"
+number = 1
+print(f"Bugun {sana} {oy} {number}-dars bo'lyapti")
+
+
+# In[8]:
+
+
+a = 25
+b = 32
+c= -12
+print(a+b+c)
+
+
+# In[9]:
+
+
+d = 45
+e = 54
+print(d*e)
+
+
+# In[11]:
+
+
+yosh = input("Yoshingizni kiriting: ")
+print("Siz " + str(int(2021)-int(yosh)) + "-yilda tavallud topgansiz!")
+
+
+# In[15]:
+
+
+print("Siz 2022-yilda " + str(int(yosh) + 1) + " ga kirasiz!")
+
+
+# In[1]:
+
+
+til = input("Siz qaysi tillarni bilasiz? ")
+print("Qoyil qoldime, " + til + "! Siz shu tillarni bilar ekansiz. Ammo o'rganishda davom eting ;)")
+
+
+# In[2]:
+
+
+son = input("Sizga qaysi raqam yoqadi? ")
+print("Sizga " + son + " yoqsa, menga esa 8 raqami yoqadi. :)")
+
+
+# In[4]:
+
+
+print("Istalgan ikki soningizni darajaga ko'tarib beramiz!")
+a = float(input("Sonni kiritiing:\n>>>  "))
+daraja = float(input("Nechanchi darajaga ko'taramiz? \n>>>"))
+print("NATIJA\n" +str(a**daraja))
+
+
+# In[ ]:
+
+
+
+
