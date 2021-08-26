86778f28fdba83e21ec3f90ff51a7903d545dc13
Oybek-Yuldoshev <88775680+Oybek-Yuldoshev@users.noreply.github.com>
2021-08-25 23:27:09 -0500

    Add files via upload

---
+++ L_python(hometask_2).py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+a = 8
+a += 80
+a /= 11
+a *= 5
+print(a)
+satr = "Salom, "
+satr+="mening ismim"
+satr+=" Oybek,"
+satr +=" yoshim 21 da"
+print(satr)
+
+
+# In[ ]:
+
+
+b = float(input("Iltimos, biror son kiritsangiz)"))
+print("Men shu sonning 11-darajasini aytaman, qizig'i bu juda ham oson)\n")
+print(str(b) + " ning 11-darajasi " + str(b**11) + " ga teng")
+
+
+# In[ ]:
+
+
+#Kutubxona
+ism = input("Salom ism familiyangizni aytsangiz")
+ism = ism.title()
+kitob = input(f"{ism} sizga qaysi kitob kerak?")
+muddat = input(f"{ism} {kitob} ni qancha muddatga olmoqchisiz? (necha kunga)")
+tamom = print(f"{ism} sizga {kitob} kitobini {muddat} ga rasmiylashitirib berdim. Iltimos vaqtida olib kelishni unutmang :)")
+
+
+# In[ ]:
+
+
+print("*********\n*\n*\n*********\n\t*\n\t*\n*********")
+
+
+# In[ ]:
+
+
+print("*********\n*\t*\n*\t*\n*********\n*\t*\n*\t*\n*********")
+
+
+# In[ ]:
+
+
+print("*********\n*\t\n*\t\n*********\n*\t*\n*\t*\n*********")
+
+
+# In[ ]:
+
+
+print("*\t*\n*\t*\n*\t*\n*********\n \t*\n \t*\n \t*")
+
+
+# In[ ]:
+
+
+ism = input("Salom ismingiz nima? ")
+a = input(f"{ism} sizga qaysi fanlar sizga yoqadi?")
+print(f"{ism} {a} fanlari chindan ham qiziq!)")
+
+
+# In[ ]:
+
+
+print("{2}ning narxi 10000 so'm, {0}ning narxi 15000 so'm, {1}ning narxi 20000 so'm, {3}ning narxini ayta olmaymiz chunki qolgani yo'q". format("olma", "banan", "gilos", "ananas"))
+
+
+# In[ ]:
+
+
+#taxi
+
+a = input("Salom sizni qaysi manzilga olib borib qoyish kerak?")
+b = input(f"{a} ga borishlik 25000 so'm bo'ladi")
+
+
+# In[ ]:
+
+
+#dokon 
+
+print("Salom do'konimizga xush kelibsiz!")
+a = input("Sizga qanday mahsulotlar kerak?")
+print(f"{a} savatga solindi!")
+print("bizda {0} 12000 so'm, {1} 15000 so'm, {2} 13000 so'm, {3} 5000 so'm". format("olma", "Coca Cola", "Pepsi", "non"))
+
+
+# In[ ]:
+
+
+
+
